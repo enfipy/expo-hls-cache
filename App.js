d025d19c60f6234daf7d18837d180d42f4fc901d
David Ackerman <enfipy@gmail.com>
2024-01-22 17:10:09 +0700

    initial commit wtf

--- App.js
+++ App.js
@@ -1,13 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import Video from 'react-native-video';
-import { StyleSheet, Text, View } from 'react-native';
+import { useRef, useEffect } from "react";
+import { StatusBar } from "expo-status-bar";
+import Video from "react-native-video";
+import { DeviceEventEmitter, StyleSheet, Text, View } from "react-native";
+import {
+  HLS_CACHING_RESTART,
+  CacheManagerProvider,
+  LFUPolicy,
+  useAsyncCache,
+} from "react-native-cache-video";
 
 export default function App() {
+  const freePolicyRef = useRef(new LFUPolicy(5));
+  const { setVideoPlayUrlBy, cachedVideoUrl } = useAsyncCache();
+
+  const uri = "https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8";
+  useEffect(() => {
+    const listener = DeviceEventEmitter.addListener(HLS_CACHING_RESTART, () => {
+      setVideoPlayUrlBy(uri);
+    });
+    return () => {
+      listener.remove();
+    };
+  }, [setVideoPlayUrlBy]);
+
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
-      <Text>Hey, it's working!</Text>
-      <Video style={styles.video} muted source={{ uri: "https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8" }} />
+      <CacheManagerProvider cachePolicy={freePolicyRef}>
+        <Text>Hey, it's working!</Text>
+        <Video
+          style={styles.video}
+          muted
+          source={{ uri: cachedVideoUrl || uri }}
+        />
+      </CacheManagerProvider>
     </View>
   );
 }
@@ -15,9 +41,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
   video: {
     height: 200,
