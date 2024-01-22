0de6351cddffe088927cfdadd3832f2004dd31c0
David Ackerman <enfipy@gmail.com>
2024-01-22 16:29:30 +0700

    feat: add expo-dev-client and video

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
+import Video from 'react-native-video';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Text>Hey, it's working!</Text>
+      <Video style={styles.video} muted source={{ uri: "https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8" }} />
     </View>
   );
 }
@@ -16,5 +18,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  video: {
+    height: 200,
+    width: 200,
   },
 });
